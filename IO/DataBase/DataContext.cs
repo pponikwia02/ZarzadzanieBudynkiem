86720357dc9ae8bf7712b50325b840e8e6d9d8ee
Michal <Michal@DESKTOP-17C4D1R>
2025-02-09 17:12:56 +0100

    Zaimplementowanie logiki rezerwacji sal oraz zapisu do bazy danych

--- IO/DataBase/DataContext.cs
+++ IO/DataBase/DataContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IO.MainApp;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +16,6 @@ namespace IO.DataBase
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Administratorzy;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;");
         }
         public DbSet<Admin> Admin { get; set; }
+        public DbSet<Sala>Sale { get; set; }
     }
 }
--- IO/DataBase/admin.cs
+++ IO/DataBase/admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace IO.DataBase
 {
     public class Admin
     {
-        
+        [Key]
         public int id { get; set; }
         public required string login { get; set; }
         public required string password { get; set; }
