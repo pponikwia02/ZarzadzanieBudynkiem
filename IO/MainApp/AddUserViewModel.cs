bc6cac3dca3cf8203da530c0ccaf08a3c82b61d8
pponikwia02 <pponikwia02@gmail.com>
2025-05-07 14:16:27 +0200

    MVVM-AUW

---
+++ IO/MainApp/AddUserViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO.DataBase;
+using System.Windows.Input;
+using System.Windows;
+
+namespace IO.MainApp
+{
+    public class AddUserViewModel : INotifyPropertyChanged
+    {
+        private string _login;
+        private string _password;
+        private string _repeatPassword;
+        private UserTypeItem _selectedUserType;
+
+        public string Login
+        {
+            get => _login;
+            set { _login = value; OnPropertyChanged(nameof(Login)); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(nameof(Password)); }
+        }
+
+        public string RepeatPassword
+        {
+            get => _repeatPassword;
+            set { _repeatPassword = value; OnPropertyChanged(nameof(RepeatPassword)); }
+        }
+
+        public UserTypeItem SelectedUserType
+        {
+            get => _selectedUserType;
+            set { _selectedUserType = value; OnPropertyChanged(nameof(SelectedUserType)); }
+        }
+
+        public ObservableCollection<UserTypeItem> UserTypes { get; }
+
+        public ICommand AddCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public Window WindowInstance { get; set; }
+
+        public AddUserViewModel()
+        {
+            UserTypes = new ObservableCollection<UserTypeItem>(UserTypeItem.GetUserTypes());
+            SelectedUserType = UserTypes.FirstOrDefault();
+
+            AddCommand = new RelayCommand(AddUser);
+            CancelCommand = new RelayCommand(CancelWindow);
+        }
+
+        private void AddUser(object obj)
+        {
+            using var context = new DataContext();
+
+            if (string.IsNullOrEmpty(Login) || string.IsNullOrEmpty(Password))
+            {
+                MessageBox.Show("Uzupełnij wszystkie pola!");
+                return;
+            }
+
+            if (Password != RepeatPassword)
+            {
+                MessageBox.Show("Podane hasła nie są identyczne");
+                return;
+            }
+
+            if (context.Users.Any(u => u.login == Login))
+            {
+                MessageBox.Show("Użytkownik z takim loginem już istnieje!");
+                return;
+            }
+
+            var hashedPassword = PasswordHasher.HashPassword(Password);
+
+            var newUser = new AppUser
+            {
+                login = Login,
+                password = hashedPassword,
+                UserType = SelectedUserType.Value
+            };
+
+            context.Users.Add(newUser);
+            context.SaveChanges();
+
+            MessageBox.Show("Rejestracja zakończona sukcesem!");
+            if (WindowInstance != null)
+            {
+                ((AddUserWindow)WindowInstance).IsReservationSuccessful = true;
+                WindowInstance.Close();
+            }
+        }
+
+        private void CancelWindow(object obj)
+        {
+            if (WindowInstance != null)
+            {
+                WindowInstance.DialogResult = false;
+                WindowInstance.Close();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
--- IO/MainApp/AddUserWindow.xaml.cs
+++ IO/MainApp/AddUserWindow.xaml.cs
@@ -1,104 +1,32 @@
 ﻿using IO.DataBase;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using IO.MainApp;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows;
 
 namespace IO.MainApp
 {
-    /// <summary>
-    /// Logika interakcji dla klasy AddUserWindow.xaml
-    /// </summary>
     public partial class AddUserWindow : Window
     {
         public bool IsReservationSuccessful { get; set; }
+
         public AddUserWindow()
         {
             InitializeComponent();
-            UserTypeComboBox.ItemsSource = UserTypeItem.GetUserTypes();
-            UserTypeComboBox.SelectedIndex = 0;//domyślnie jest wykładowca
-            UserTypeComboBox.SelectedValuePath = "Value";
-            UserTypeComboBox.DisplayMemberPath = "DisplayName";
-        }
-        public class UserService
-        {
-            public bool RegisterUser(string login, string password, string passwordRepeat, UserTypeItem selectedType)
-            {
 
-                DataContext context = new DataContext();
-                if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
-                {
-                    MessageBox.Show("Uzupełnij wszystkie pola!");
-                    return false;
-                }
-
-                if (PasswordHasher.HashPassword(password) != PasswordHasher.HashPassword(passwordRepeat))
-                {
-                    MessageBox.Show("Podane hasła nie są identyczne");
-                    return false;
-                }
-                if (selectedType == null)
-                {
-                    MessageBox.Show("Wybierz typ użytkownika");
-                    return false;
-                }
-
-                {
-                    // Sprawdzenie, czy login już istnieje w bazie
-                    if (context.Users.Any(u => u.login == login))
-                    {
-                        MessageBox.Show("Użytkownik z takim loginem już istnieje!");
-                        return false;
-                    }
-
-                    // Tworzenie użytkownika
-                    var hashedpassword = PasswordHasher.HashPassword(password);
-                    var newUser = new AppUser
-                    {
-                        login = login,
-                        password = hashedpassword, // Hashujemy hasło przed zapisem!
-                        UserType = selectedType.Value
-                    };
-
-                    context.Users.Add(newUser);
-                    context.SaveChanges(); // Zapis do bazy
-
-                    MessageBox.Show("Rejestracja zakończona sukcesem!");
-                    return true;
-                }
-            }
+            if (DataContext is AddUserViewModel vm)
+                vm.WindowInstance = this;
         }
 
-        private void AddButton_Click(object sender, RoutedEventArgs e)
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            var userService = new UserService();
-            var selectedUserType = (UserTypeItem)UserTypeComboBox.SelectedItem;
-
-            string login = Login_Box.Text;
-            //string password = Password_Box.Password;
-            //string password_repeat = Password_Box_Repeat.Password;
-            string password = PasswordHasher.HashPassword(Password_Box.Password);
-            string password_repeat = PasswordHasher.HashPassword(Password_Box_Repeat.Password);
-
-            if (userService.RegisterUser(login, password, password_repeat, selectedUserType))
-            {
-                IsReservationSuccessful = true;
-                Close(); 
-            }
+            if (DataContext is AddUserViewModel vm)
+                vm.Password = ((PasswordBox)sender).Password;
         }
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+
+        private void RepeatPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            DialogResult = false;
-            Close();
+            if (DataContext is AddUserViewModel vm)
+                vm.RepeatPassword = ((PasswordBox)sender).Password;
         }
     }
 }
