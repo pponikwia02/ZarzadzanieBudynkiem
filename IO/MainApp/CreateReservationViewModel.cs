f1bccd8d020103e133f1b2131d105375fe0a6037
pponikwia02 <pponikwia02@gmail.com>
2025-05-08 10:32:57 +0200

    MVVM -> CRW

---
+++ IO/MainApp/CreateReservationViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO.DataBase;
+using System.Windows.Input;
+using System.Windows;
+
+namespace IO.MainApp
+{
+    public class CreateReservationViewModel : INotifyPropertyChanged
+    {
+        private string _nrSali;
+        public string NrSali
+        {
+            get => _nrSali;
+            set
+            {
+                _nrSali = value;
+                OnPropertyChanged(nameof(NrSali));
+            }
+        }
+
+        private string _rezerwujacy;
+        public string Rezerwujacy
+        {
+            get => _rezerwujacy;
+            set
+            {
+                _rezerwujacy = value;
+                OnPropertyChanged(nameof(Rezerwujacy));
+            }
+        }
+
+        private string _od;
+        public string Od
+        {
+            get => _od;
+            set
+            {
+                _od = value;
+                OnPropertyChanged(nameof(Od));
+            }
+        }
+
+        private string _do;
+        public string Do
+        {
+            get => _do;
+            set
+            {
+                _do = value;
+                OnPropertyChanged(nameof(Do));
+            }
+        }
+
+        public ICommand CreateCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public event Action<bool> RequestClose;
+
+        public CreateReservationViewModel()
+        {
+            CreateCommand = new RelayCommand(_ => CreateReservation());
+            CancelCommand = new RelayCommand(_ => RequestClose?.Invoke(false));
+        }
+
+        private void CreateReservation()
+        {
+            if (string.IsNullOrWhiteSpace(NrSali) || string.IsNullOrWhiteSpace(Rezerwujacy) || string.IsNullOrWhiteSpace(Od))
+            {
+                MessageBox.Show("Proszę uzupełnić wszystkie pola.");
+                return;
+            }
+
+            using var context = new DataContext();
+
+            bool exists = context.Sale.Any(s => s.NrSali == NrSali && s.Od == Od);
+            if (exists)
+            {
+                MessageBox.Show("Sala jest już zarezerwowana na ten dzień.");
+                return;
+            }
+
+            var newReservation = new Sala
+            {
+                NrSali = NrSali,
+                Rezerwujacy = Rezerwujacy,
+                Od = Od,
+                Do = Do
+            };
+
+            context.Sale.Add(newReservation);
+            context.SaveChanges();
+
+            MessageBox.Show("Rezerwacja zapisana!");
+            RequestClose?.Invoke(true);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string prop) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
--- IO/MainApp/CreateReservationWindow.xaml.cs
+++ IO/MainApp/CreateReservationWindow.xaml.cs
@@ -22,75 +22,22 @@ namespace IO.MainApp
     /// </summary>
     public partial class CreateReservationWindow : Window
     {
-        // Propercja do przechowywania nowej rezerwacji
-        //okno do rezerwacji
-        public Classroom NewReservation { get;   set; }
-        public bool IsReservationSuccessful {  get; set; }
+        public bool IsReservationSuccessful { get; private set; }
 
         public CreateReservationWindow()
         {
             InitializeComponent();
-        }
-        public class SalaRezerwacjaHandler
-        {
-            public static bool ZapiszRezerwacje(string NrSali,string Rezerwujacy,string Od,string Do)
-            {
-                DataContext context = new DataContext();
-                {
-                    // Sprawdzenie, czy sala jest już zajęta w danym dniu
-                    bool istnieje = context.Sale.Any(s => s.NrSali == NrSali && s.Od== Od);
-                    if (istnieje)
-                    {
-                        return false; // Sala jest już zajęta
-                    }
 
-                    var nowaRezerwacja = new Sala
-                    {
-                        NrSali = NrSali,
-                        Rezerwujacy=Rezerwujacy,
-                        Od = Od,
-                        Do = Do
-                    };
+            if (DataContext is not CreateReservationViewModel vm)
+                DataContext = new CreateReservationViewModel();
 
-                    context.Sale.Add(nowaRezerwacja);
-                    context.SaveChanges();
-                    return true;
-                }
-               
-            }
-        }
-        private void CreateButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            string NrSali=ClassroomIdTextBox.Text;
-            string rezerwujacy=ClassroomNameTextBox.Text;
-            string Od=ReservationStartTextBox.Text;
-            string Do=ReservationEndTextBox.Text;
-
-            if (string.IsNullOrEmpty(NrSali) || string.IsNullOrEmpty(rezerwujacy) || Od == null)
-            {
-                MessageBox.Show("Proszę uzupełnić wszystkie pola.");
-                return;
-            }
-
-            bool sukces = SalaRezerwacjaHandler.ZapiszRezerwacje(NrSali,rezerwujacy,Od,Do);
-
-            if (sukces)
-            {
-                IsReservationSuccessful = true;
-                MessageBox.Show("Rezerwacja zapisana!");
-                Close();
-            }
-            else
-            {
-                MessageBox.Show("Sala jest już zarezerwowana na ten dzień.");
-            }
-
+            (DataContext as CreateReservationViewModel).RequestClose += OnRequestClose;
         }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        private void OnRequestClose(bool success)
         {
-            DialogResult = false;
+            IsReservationSuccessful = success;
+            DialogResult = success;
             Close();
         }
     }
