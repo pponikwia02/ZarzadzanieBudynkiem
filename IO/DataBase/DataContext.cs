efbb1b14eb516f48416ef0d0ce9caf3b5f721f38
Michal <Michal@DESKTOP-17C4D1R>
2025-05-22 12:20:17 +0200

    CodeFirst

--- IO/DataBase/DataContext.cs
+++ IO/DataBase/DataContext.cs
@@ -13,7 +13,9 @@ namespace IO.DataBase
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Administratorzy;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;");
+            var connectionstring="Data Source=localhost;Initial Catalog=Administratorzy;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;";
+            optionsBuilder.UseSqlServer(connectionstring);
+            base.OnConfiguring(optionsBuilder);
         }
         public DbSet<AppUser>Users { get; set; }
         public DbSet<Sala>Sale { get; set; }
